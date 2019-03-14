c31c66c5bc5d93c9ab9c5bfbb0c633ece5434803
tialistia <tialistia93@gmail.com>
2019-03-14 15:05:59 +0700

    Add files via upload

---
+++ 4210161024/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UDPServer
+{
+
+
+    public static void Main()
+    {
+
+        byte[] data = new byte[1024];
+        IPEndPoint ip = new IPEndPoint(IPAddress.Any, 9050);
+        UdpClient newsock = new UdpClient(ip);
+
+        Console.WriteLine("Menunggu Client...");
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+        data = newsock.Receive(ref sender);
+
+        Console.WriteLine("pesan diterima dari {0}:", sender.ToString());
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+        string welcome = "Selamat datang di server";
+        data = Encoding.ASCII.GetBytes(welcome);
+        newsock.Send(data, data.Length, sender);
+
+        while (true)
+        {
+
+
+            data = newsock.Receive(ref sender);
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+            newsock.Send(data, data.Length, sender);
+
+        }
+
+    }
+
+}
---
+++ 4210161024/client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UdpClientSample
+{
+    public static void Main()
+    {
+        byte[] data = new byte[1024];
+        string input, stringData;
+        UdpClient server = new UdpClient("192.168.43.181", 9050);
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+        string welcome = "Hallo";
+        data = Encoding.ASCII.GetBytes(welcome);
+        server.Send(data, data.Length);
+
+        data = server.Receive(ref sender);
+
+        Console.WriteLine("pesan diterima dari {0}:", sender.ToString());
+        stringData = Encoding.ASCII.GetString(data, 0, data.Length);
+
+
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (input == "exit")
+                break;
+
+            server.Send(Encoding.ASCII.GetBytes(input), input.Length);
+            data = server.Receive(ref sender);
+            stringData = Encoding.ASCII.GetString(data, 0, data.Length);
+
+        }
+        Console.WriteLine("Stopping client");
+        server.Close();
+    }
+}
+
